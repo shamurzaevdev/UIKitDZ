9a80c6eddd4b63ed5e74b02ffbd12259e7d5c48c
Ali <alisham93@bk.ru>
2022-09-26 08:11:57 +0200

    nifiga ne rabotaet, hotya vse norm delal

---
+++ UIKitDZ/UIKitDZ/MainMenuViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Эл on 21.09.2022.
+//
+
+import UIKit
+
+class MainMenuViewConroller: UIViewController {
+    
+}
---
+++ UIKitDZ/UIKitDZ/PlayerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerViewController.swift
+//  UIKitDZ
+//
+//  Created by Эл on 26.09.2022.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerViewController: UIViewController {
+
+   
+
+}
