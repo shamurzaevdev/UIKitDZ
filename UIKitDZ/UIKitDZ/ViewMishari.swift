7650065539140162194fc8919136303dfd2c54d7
Ali <alisham93@bk.ru>
2022-09-27 22:31:29 +0200

    Сделал дз 6

--- UIKitDZ/UIKitDZ/ViewMishari.swift
+++ UIKitDZ/UIKitDZ/ViewMishari.swift
@@ -25,18 +25,18 @@ class ViewMishari: UIViewController {
                 let urlString = Bundle.main.path(forResource: "Fatihah", ofType: "mp3")
             do {
                 try AVAudioSession.sharedInstance().setMode(.default)
-                try AVAudioSession.sharedInstance().setActive(true,     options: .notifyOthersOnDeactivation)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
                 guard let urlString = urlString else {
                     return
                 }
-                player = try AVAudioPlayer(contentsOf:              URL(fileURLWithPath: urlString))
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
                 guard let player = player else {
                     return
                 }
                 player.play()
             }
             catch {
-                print("something went wrong")
+                print("something went wrong!")
             }
         }
     }
--- UIKitDZ/UIKitDZ/ViewSeferagic.swift
+++ UIKitDZ/UIKitDZ/ViewSeferagic.swift
@@ -24,18 +24,18 @@ class ViewSeferagic: UIViewController {
             let urlString = Bundle.main.path(forResource: "Al-Anbiya", ofType: "mp3")
         do {
             try AVAudioSession.sharedInstance().setMode(.default)
-            try AVAudioSession.sharedInstance().setActive(true,     options: .notifyOthersOnDeactivation)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
             guard let urlString = urlString else {
                 return
             }
-            player = try AVAudioPlayer(contentsOf:              URL(fileURLWithPath: urlString))
+            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
             guard let player = player else {
                 return
             }
             player.play()
         }
         catch {
-            print("something went wrong")
+            print("something went wrong!")
         }
     }
     }
